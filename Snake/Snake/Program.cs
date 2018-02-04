4520cdc7832d7c031722c44252503b71d5c65cf2
Vladimir Savchenko <savorigin@outlook.com>
2018-02-04 22:46:13 +0200

    Движение змейки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Initialize()
         {
-            Console.SetBufferSize( 80, 25 );
+            Console.SetBufferSize(80, 25);
 
             // Отрисовка рамочки
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
@@ -22,13 +23,36 @@ namespace Snake
             downLine.Draw();
             leftLine.Draw();
             rightLine.Draw();
+        }
+
+        static void Main(string[] args)
+        {
+            Initialize();
 
             // Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-
-            Console.ReadLine();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
         }
     }
 }
